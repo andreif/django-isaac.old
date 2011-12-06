66049c1e4fb32b04e9b0d7e1625fa7ea808e8a55
Isaac Bythewood <isaac@bythewood.me>
2011-12-06 00:37:17 -0500

    Attempting to serve static files through Django, silly Heroku.

--- django_isaac/urls.py
+++ django_isaac/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+
 
 urlpatterns = patterns('',
     url(r'^', include('core.urls')),
+    url(r'^(?P<path>static/.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
 )
