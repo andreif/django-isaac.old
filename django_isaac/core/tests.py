1ffbc2883a71b01eebdb6eb3a4d05a4c91ec643d
Isaac Bythewood <isaac@bythewood.me>
2012-01-20 19:52:52 -0500

    Fix mobile view in base template, improve spacing in tests file.

--- django_isaac/core/tests.py
+++ django_isaac/core/tests.py
@@ -1,18 +1,22 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.core import mail
+
 import re
 import urllib2
+
 
 class DjangoIsaacTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         self.pages = ['/', '/bluescreen.html']
 
+
     def test_pages(self):
         for page in self.pages:
             response = self.client.get(page)
             self.assertEqual(response.status_code, 200)
+
 
     def test_foreign_links(self):
         for page in self.pages:
@@ -22,6 +26,7 @@ class DjangoIsaacTestCase(TestCase):
             for link in links:
                 response = urllib2.urlopen(link)
                 self.assertEqual(response.code, 200)
+
 
     def test_send_message(self):
         self.assertEqual(len(mail.outbox), 0)
